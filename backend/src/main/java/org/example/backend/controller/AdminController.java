619c186bdbc116d70c60a316ccb14f0966f30ace
Azizbek <aziz.ahadovv@yandex.ru>
2025-08-30 22:44:12 +0500

    add

--- backend/src/main/java/org/example/backend/controller/AdminController.java
+++ backend/src/main/java/org/example/backend/controller/AdminController.java
@@ -1,7 +1,29 @@
 package org.example.backend.controller;
 
+import org.example.backend.Db;
+import org.example.backend.entity.Role;
+import org.example.backend.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/admin")
+
 public class AdminController {
 
-    
+
+    @GetMapping("/checking")
+    public ResponseEntity<User> user() {
+        return Db.users.stream()
+                .filter(u -> u.getRole().equals(Role.ADMIN))
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
 }
